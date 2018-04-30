0014e9adb2cef7a24f1c88cce20f8049387667fb
vikas garud <vikas.garud@ibm.com>
2018-04-29 22:25:30 -0500

    generated tokens

--- scripts/publishCoverage.sh
+++ scripts/publishCoverage.sh
@@ -82,5 +82,6 @@ touch abdf.asdf
 git add -f .
 #git add $FILENAME_TO_DEPLOY....
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER , deply coverage pages"
-git push --set-upstream origin master
+#git push --set-upstream origin master
+git push
 
